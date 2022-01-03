76d040025595746e34feafe81ae70aad00e5c112
segu23 <segundoschmidt@gmail.com>
2022-01-03 12:22:34 -0300

    v3.0

--- src/main/java/org/kayteam/chunkloader/commands/Command_ChunkLoader.java
+++ src/main/java/org/kayteam/chunkloader/commands/Command_ChunkLoader.java
@@ -22,22 +22,38 @@ public class Command_ChunkLoader implements CommandExecutor, TabCompleter {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, String[] args) {
         if(sender instanceof Player){
             Player player = (Player) sender;
-            if(!player.hasPermission("chunkloader.chunkloader")){
-                plugin.messages.sendMessage(player, "command.no-permissions");
-                return false;
-            }
             if(args.length>0){
                 if(args[0].equalsIgnoreCase("help")){
                     new Command_Help(plugin).chunkHelp(player);
                 }else if(args[0].equalsIgnoreCase("list")){
+                    if(!player.hasPermission("chunkloader.list")){
+                        plugin.messages.sendMessage(player, "command.no-permissions");
+                        return false;
+                    }
                     new Command_List(plugin).chunkList(player);
                 }else if(args[0].equalsIgnoreCase("reload")){
+                    if(!player.hasPermission("chunkloader.reload")){
+                        plugin.messages.sendMessage(player, "command.no-permissions");
+                        return false;
+                    }
                     new Command_Reload(plugin).commandReload(player);
                 }else if(args[0].equalsIgnoreCase("on")){
+                    if(!player.hasPermission("chunkloader.on")){
+                        plugin.messages.sendMessage(player, "command.no-permissions");
+                        return false;
+                    }
                     new Command_On(plugin).enableChunkLoad(player);
                 }else if(args[0].equalsIgnoreCase("off")){
+                    if(!player.hasPermission("chunkloader.off")){
+                        plugin.messages.sendMessage(player, "command.no-permissions");
+                        return false;
+                    }
                     new Command_Off(plugin).disableChunkLoad(player);
                 }else if(args[0].equalsIgnoreCase("tp")){
+                    if(!player.hasPermission("chunkloader.tp")){
+                        plugin.messages.sendMessage(player, "command.no-permissions");
+                        return false;
+                    }
                     if(args.length>1){
                         try{
                             new Command_TP(plugin).chunkTeleport(player,Integer.parseInt(args[1]));
@@ -46,6 +62,10 @@ public class Command_ChunkLoader implements CommandExecutor, TabCompleter {
                         plugin.messages.sendSimpleMessage(player, "&c/cl tp <list-number>");
                     }
                 }else if(args[0].equalsIgnoreCase("menu")){
+                    if(!player.hasPermission("chunkloader.menu")){
+                        plugin.messages.sendMessage(player, "command.no-permissions");
+                        return false;
+                    }
                     new Command_Menu(plugin).openMenu(player);
                 }else{
                     new Command_Help(plugin).chunkHelp(player);
